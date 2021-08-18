6afcb8c5dd5970487c2c1ad0e1898bd306d06015
Raia Kisseljova <84378854+Raissa-Kisseljova@users.noreply.github.com>
2021-08-18 22:57:27 +0100

    Backend of blog api

---
+++ services/fs-tools/fs-tools.js
@@ -0,0 +1,4 @@
+import fs from "fs-extra";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+const { readJSON, writeJSON, writeFile } = fs;
---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import { join } from "path";
+import process from "process";
+import blogPostsRouter from "../services/blogPosts/blogPosts.js";
+const server = express();
+const port = 3001;
+
+const publicFolderPath = join(process.cwd(), "public");
+
+server.use("/public", express.static(publicFolderPath));
+server.use(cors());
+server.use(express.json());
+
+server.use("/blogPosts", blogPostsRouter);
+
+server.listen(3001, console.log("server is running"));
